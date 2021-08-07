7c9d5364978a472c45a5aee2b7b98906bfe0dfa9
lucianoavila90 <lucianothadeuavila@gmail.com>
2021-08-07 19:56:36 -0300

    server com nodemon

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+
+app.get('/',function(req,res){
+    res.json({message:'Hello World'});
+});
+
+app.listen(port,function(){
+    console.log(`Server running on port ${port}`);
+});
