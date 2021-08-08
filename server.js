13e32cb235d3f899b306a1d788dfa8fcbcdab97a
lucianoavila90 <lucianothadeuavila@gmail.com>
2021-08-08 10:55:29 -0300

    server up

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 27017;
+
+var linkConexao = 'mongodb://localhost:27017/react_project'
+
+mongoose.connect(linkConexao,{
+    useUnifiedTopology:true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+},function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log('Conectado com sucesso!');
+    }
+})
 
 app.use(cookieParser());
 app.use(cors());
